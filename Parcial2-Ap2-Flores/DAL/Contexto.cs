64cb7ca62f507b0b26d50e27639323b712e43779
Jose Armando Flores Baldera <jose_flores3@ucne.edu.do>
2020-07-07 22:04:21 -0400

    Creando contexto

--- Parcial2-Ap2-Flores/DAL/Contexto.cs
+++ Parcial2-Ap2-Flores/DAL/Contexto.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Parcial2_Ap2_Flores.DAL
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = DATA\.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
     }
 }
